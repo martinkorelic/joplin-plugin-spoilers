70847a5a15f3188354ea82f42ad27564b6b09111
martinkorelic <martin.korelic@protonmail.com>
2021-04-13 21:52:22 +0200

    Version 0.3.0 - new format

--- README.md
+++ README.md
@@ -3,42 +3,31 @@
 This Joplin plugin allows you to create cards with title and extendable body.
 
 **Note**: Requires Joplin 1.7.0+
+**Version**: 0.3.0
 
 ![](./docs/cards-plugin-preview.gif)
 
 ## Usage
 
 In order to create a card, you need to write in this format:
-``````
-```card
-
-Card name here...
+```
 :[
-    Card body text here...
+Card name here...
+
+Card body text here...
+
 ]:
-
 ```
-``````
+Please note, that the empty line above and below card body text is **needed**.
 
-Card body supports markdown formatting as well, but **NOTE** that normal code blocks with 3 backticks (```` ``` ````) **DO NOT WORK!**
-
-Instead when writing code block use (`~~~`):
-```
-~~~js
-
-function something() {
-    return null;
-}
-
-~~~
-```
+Card body supports markdown formatting as well.
 
 ### Example
 
-``````
-```card
-3 ways to check if an Object has a property in JS
+```
 :[
+3 ways to check if an Object has a property in JS
+
 Using:
 1. `hasOwnProperty()` method
 2. `in` operator
@@ -55,15 +44,13 @@ hero.toString; // => function() {...}
 hero.hasOwnProperty('toString'); // => false
 ~~~
 * * *
-....
+
 ]:
 ```
-``````
 
 ## Custom styling of cards
 
 If you would like to style the spoiler cards to your preference, use the following in your `userstyle.css` file:
-
 
 ```css
 /* Styling of the card title */
@@ -80,6 +67,6 @@ If you would like to style the spoiler cards to your preference, use the followi
 ## Notes
 
 - I have not thoroughly tested the plugin, so note that **there might be bugs**.
-- I might have to change formatting in the future to improve highlighting in text editor.
+- I might have to change formatting in the future to improve highlighting in text editor, but nothing significant from now on.
 
 > Created on 12th April 2021
--- src/cards.js
+++ src/cards.js
@@ -5,15 +5,15 @@ module.exports = {
                 const pluginId = context.pluginId;
                 const contentScriptId = _options.contentScriptId;
 
-                const defaultRender = markdownIt.renderer.rules.fence || function(tokens, idx, options, env, self) {
-                    return self.renderToken(tokens, idx, options, env, self);
-                };
-
-                markdownIt.renderer.rules.fence = function (tokens, idx, options, env, self) {
+                markdownIt.block.ruler.after('fence', 'spoiler_card', spoiler_card, {alt: ['paragraph', 'reference', 'blockquote', 'list']});
+                
+                //markdownIt.renderer.rules.spoiler_card = spoiler_card_renderer;
+                /*
+                markdownIt.renderer.rules.text = function (tokens, idx, options, env, self) {
                     let token = tokens[idx];
 
                     // We detect if card block
-                    if (token.info !== 'card') return defaultRender(tokens, idx, options, env, self);
+                    if (token.tag !== "p") return defaultRender(tokens, idx, options, env, self);
 
                     // Split the card title and body by :[]:
                     let card = token.content.match(/(?<title>.+)\n:\[(?<body>(?:.|\n)*?)\]:/i);
@@ -33,7 +33,7 @@ module.exports = {
                         ${markdownIt.render(body)}
                         </div>
                     </details>`;
-                }
+                }*/
 
             },
             // Assests such as JS or CSS that should be loaded in the rendered HTML document
@@ -46,4 +46,109 @@ module.exports = {
             },
         }
     }
+}
+
+function spoiler_card(state, start, end, silent) {
+
+    let lastPos, found = false,
+    pos = state.bMarks[start]+ state.tShift[start],
+    max = state.eMarks[start],
+    next;
+
+    var token;
+    let curLine = start;
+
+    let firstLine;
+    if (pos + 2 > max) {
+		return false;
+	}
+
+    // Check when it starts with ':['
+    if (state.src.slice(pos, pos+2) !== ':[') return false;  
+    pos += 2;
+
+    // We don't accept empty card formats
+    if (state.src.slice(pos, pos+2) == ']:') return false;
+    pos += 2;
+
+    // Check if line is not empty
+    firstLine = state.src.slice(pos, max);
+
+    if (silent) return true;    
+
+    curLine++;
+    // Correct formatting of the title
+    if (state.isEmpty(curLine)) return false;
+
+    let title = curLine;
+
+    curLine++;
+    // Needs to be atleast one empty line in between for better formatting
+    if (!state.isEmpty(curLine)) return false;
+
+    curLine++;
+    // Now there needs to be atleast some content before we render the card
+    if (state.isEmpty(curLine)) {
+        
+        return false;
+    }
+
+    // If the formatting is okay, we create new tokens
+    /*
+    1 means the tag is opening
+    0 means the tag is self-closing
+    -1 means the tag is closing
+    */
+    
+    state.push('details_open', 'details', 1);
+
+    console.log(state);
+
+    token = state.push('summary_open', 'summary', 1);
+    token.attrs = [[ 'class', 'summary-title' ]];
+
+    token = state.push('inline', '', 0);
+    token.map = [ title, title ];
+    token.content = state.getLines(title, title+1, state.tShift[title], false).trim();
+    token.children = [];
+
+    state.push('summary_close', 'summary', -1);
+
+    token = state.push('spoiler_card_body', 'div', 1);
+    token.attrs = token.attr = [[ 'class', 'summary-content' ]];
+
+    // Content starts
+    for (next = curLine; !found;) {
+        next++;
+        
+        if (next >= end) {
+            break;
+        }
+        
+        pos = state.bMarks[next] + state.tShift[next];
+        max = state.eMarks[next];
+        
+        if (pos < max && state.tShift[next] < state.blkIndent) {
+            break;
+        }
+
+        // Check if there's only ']:' on the line
+        if (state.src.slice(pos, max).length == 2 && state.src.slice(pos, max).trim().slice(-2) == ']:') {
+            lastPos = state.src.slice(0, max).lastIndexOf(']:');
+            lastLine = state.src.slice(pos, lastPos);
+            found = true;
+        }
+    }
+
+    // We use to render markdown within
+    state.md.block.tokenize(state, curLine, next, false);
+    
+    state.line = next + 1;
+
+    // Finalize body
+    state.push('spoiler_body_close', 'div', -1);
+
+    // Finalize details    
+    state.push('details_close', 'details', -1);
+    return true;
 }
